1db6aa2121b938d446281acf3e1742202ccded6e
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-11-08 23:58:53 -0400

    implemeting models with TDD

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+    it "validate presence of required fields" do
+      should validate_presence_of(:title)
+      should validate_presence_of(:content)
+      should validate_presence_of(:published)
+    end
+  end
 end
